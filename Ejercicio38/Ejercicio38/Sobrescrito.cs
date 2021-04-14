179ee79ebfccf8713efcaf79c0bd7f32c63c43a8
imettaUTN <31007863+imettaUTN@users.noreply.github.com>
2021-04-13 21:36:25 -0300

    Ejercicio 38/39

---
+++ Ejercicio38/Ejercicio38/Sobrescrito.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio38
+{
+    public abstract class Sobrescrito
+    {
+        protected string miAtributo;
+        public Sobrescrito()
+        {
+            this.miAtributo = "Probar abstractos";
+        }
+        public override string ToString()
+        {
+            return "Esto es mi metodo ToString sobreescrito";
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is Sobrescrito;
+        }
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+
+        public abstract string MiPropiedad
+        {
+            get;
+        }
+
+        public abstract string MiMetodo();
+    }
+}
